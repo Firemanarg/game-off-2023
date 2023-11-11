b5f8b74545f136f99e91a35913d179c0527b85a5
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-11-11 10:10:01 -0300

    feat(Online): implement auth by device_id

--- autoload/online.gd
+++ autoload/online.gd
@@ -6,6 +6,15 @@ const Host: String = "127.0.0.1"
 const Port: int = 7350
 const ServerKey: String = "defaultkey"
 
+## Response of auth functions.
+enum AuthResponse {
+	SUCCESS,		## Successfully auth.
+	INVALID_AUTH,	## Invalid field of auth attempt.
+	PREV_REG_EMAIL,	## Email was registered previously.
+	SOCKET_ERROR,	## Error when connecting socket.
+}
+
+
 var session: NakamaSession = null
 var account: NakamaAPI.ApiAccount = null
 
@@ -24,4 +33,15 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	pass
+
+
+func device_auth(username = null) -> AuthResponse:
+	var device_id: String = OS.get_unique_id()
+
+	session = await client.authenticate_device_async(device_id, username)
+	if session.is_exception():
+		var exception: NakamaException = session.get_exception()
+		print("[auth_error]: ", exception.message)
+		return AuthResponse.INVALID_AUTH
+	return AuthResponse.SUCCESS
 
