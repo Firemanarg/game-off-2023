a42d43ec542b68756b3ef91c888bc5c1d11dd845
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-11-13 15:57:31 -0300

    feat(OnlineMatch): implement match creation

--- autoload/online_match.gd
+++ autoload/online_match.gd
@@ -1,19 +1,41 @@
 extends Node
 
 
+signal match_created(match_code)
+
+var match_code: String = ""
+
 @onready var match_: NakamaRTAPI.Match = null
 @onready var matchmaker_ticket: NakamaRTAPI.MatchmakerTicket = null
-@onready var match_presences: Array[NakamaRTAPI.UserPresence] = []
+
+## [code]key[/code]: [param user_id] as [String]
+## | [code]value[/code]: [param presence] as [code]NakamaRTAPI.UserPresence[/code]
+@onready var match_presences: Dictionary = {}
 
 
 func _ready() -> void:
 	pass
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	pass
+
+
+func create_match() -> void:
+	print("[match]: function create called")	# DEBUG
+	var response: NakamaAPI.ApiRpc = await Online.call_rpc_func("generate_match_code")
+	if response.is_exception():
+		print("[match_creation]: error while generating match code")
+		return
+	match_code = JSON.parse_string(response.payload).get("match_code")
+	print("[match]: generated code: ", match_code)
+	OnlineMatch.match_ = await Online.socket.create_match_async(match_code)
+	if OnlineMatch.match_.is_exception():
+		pass
+	match_created.emit(match_code)
+	return
 
