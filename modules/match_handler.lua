0b116afa531ed7674488cc1160e9bc4ed3097424
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-11-16 15:11:35 -0300

    feat(MatchHandler): implement match handler and match utils

---
+++ modules/match_handler.lua
@@ -0,0 +1,80 @@
+local nk = require("nakama")
+local utils = require("match_utils")
+local gen = require("generator")
+
+local M = {}
+
+function M.match_init(context, params)
+	local match_code = params.match_code
+	nk.logger_info(string.format("Match code: %s", tostring(match_code)))
+	local state = {
+		presences = {},
+		match_code = match_code
+	}
+	local tick_rate = 1
+	local label = "code=" .. match_code
+
+	return state, tick_rate, label
+end
+
+function M.match_join(context, dispatcher, tick, state, presences)
+	-- Presences format:
+	-- {
+	--   {
+	--     user_id = "user unique ID",
+	--     session_id = "session ID of the user's current connection",
+	--     username = "user's unique username",
+	--     node = "name of the Nakama node the user is connected to"
+	--   },
+	--  ...
+	-- }
+	for _, presence in ipairs(presences) do
+		state.presences[presence.session_id] = presence
+	end
+	return state
+  end
+
+function M.match_join_attempt(context, dispatcher, tick, state, presence, metadata)
+	local acceptuser = true
+	-- Presence format:
+	-- {
+	--   user_id = "user unique ID",
+	--   session_id = "session ID of the user's current connection",
+	--   username = "user's unique username",
+	--   node = "name of the Nakama node the user is connected to"
+	-- }
+	return state, acceptuser
+end
+
+-- function M.match_join(context, dispatcher, tick, state, presences)
+--   for _, presence in ipairs(presences) do
+--     state.presences[presence.session_id] = presence
+--   end
+
+--   return state
+-- end
+
+function M.match_leave(context, dispatcher, tick, state, presences)
+	for _, presence in ipairs(presences) do
+		state.presences[presence.session_id] = nil
+	end
+
+	return state
+end
+
+function M.match_loop(context, dispatcher, tick, state, messages)
+	return state
+end
+
+function M.match_terminate(context, dispatcher, tick, state, grace_seconds)
+  local message = "Server shutting down in " .. grace_seconds .. " seconds"
+  dispatcher.broadcast_message(2, message)
+
+  return nil
+end
+
+function M.match_signal(context, dispatcher, tick, state, data)
+  return state, "signal received: " .. data
+end
+
+return M
---
+++ modules/match_utils.lua
@@ -0,0 +1,73 @@
+local nk = require("nakama")
+local gen = require("generator")
+
+local function _register_match_code(match_code, match_id)
+	local data = {
+		collection = "RegisteredMatches",
+		key = match_code,
+		user_id = nil,
+		value = {match_id = match_id},
+		permission_read = 2,
+		permission_write = 0
+	}
+	nk.storage_write({data})
+	nk.logger_info(string.format(
+		"Registered match '%s' with code '%s'.", match_id, match_code))
+end
+
+local function _get_registered_match(match_code)
+	nk.logger_info(string.format("Attempting to get match with code %s", match_code))
+	local data = {
+		collection = "RegisteredMatches",
+		key = match_code,
+		user_id = nil
+	}
+	local result = nk.storage_read({data})
+	if table.getn(result) > 0 then
+		local value = result[1].value
+		local match_id = value.match_id
+		return match_id
+	else
+		return nil
+	end
+end
+
+local function create_match(context, _)
+	local success, result = pcall(gen.generate_match_code)
+	if not success then
+		nk.logger_info(string.format("Failed request %q", result))
+		error("Unable to generate match code")
+	else
+		local match_code = result.match_code
+		local modulename = "match_handler"
+		local initialstate = {match_code = match_code}
+		local match_id = nk.match_create(modulename, initialstate)
+		_register_match_code(match_code, match_id)
+		local result = {
+			match_id = match_id,
+			match_code = match_code
+		}
+		return nk.json_encode(result)
+	end
+end
+
+local function get_match_id_by_code(_, payload)
+	local json = nk.json_decode(payload)
+	local match_code = json.match_code
+	local match_id = _get_registered_match(match_code)
+	if match_id == nil then
+		nk.logger_info(string.format("Match with code %s not found!", match_code))
+		error(string.format("Match with code %s not found!", match_code))
+		match_code = nil
+	end
+	local result = {
+		match_id = match_id,
+		match_code = match_code
+	}
+	nk.logger_info(string.format(
+		"Returning result: match_id='%s' | match_code='%s'", match_id, match_code))
+	return nk.json_encode(result)
+end
+
+nk.register_rpc(create_match, "create_match")
+nk.register_rpc(get_match_id_by_code, "get_match_id_by_code")
