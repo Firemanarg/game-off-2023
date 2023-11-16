35bcae915ce613577db931dc95558b28ec3ed068
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-11-16 15:06:27 -0300

    feat(OnlineMatch): implement join_match function

--- autoload/online_match.gd
+++ autoload/online_match.gd
@@ -2,6 +2,9 @@ extends Node
 
 
 signal match_created(match_code)
+signal match_creation_failed()
+signal match_joined()
+signal match_join_failed()
 signal match_presences_changed()
 
 var match_code: String = ""
@@ -27,20 +30,62 @@ func _physics_process(_delta: float) -> void:
 
 
 func create_match() -> void:
-	print("[match]: function create called")	# DEBUG
-	var response: NakamaAPI.ApiRpc = await Online.call_rpc_func("generate_match_code")
+	var response: NakamaAPI.ApiRpc = await Online.call_rpc_func("create_match")
 	if response.is_exception():
-		print("[match_creation]: error while generating match code")
+		print("[match_error]: >", response.get_exception().message)
+		match_creation_failed.emit()
 		return
-	match_code = JSON.parse_string(response.payload).get("match_code")
-	print("[match]: generated code: ", match_code)
-	OnlineMatch.match_ = await Online.socket.create_match_async(match_code)
-	if OnlineMatch.match_.is_exception():
-		pass
+	var json = JSON.parse_string(response.payload)
+	var match_id: String = json.get("match_id")
+	var match_code: String = json.get("match_code")
+	await _join_match_by_id(match_id, false)
 	match_created.emit(match_code)
-	print("[match]: created successfully!")
 	Online.socket.received_match_presence.connect(_on_received_match_presence)
-	return
+
+
+func _join_match_by_id(match_id: String, emit_signals: bool = true) -> bool:
+	match_code = ""
+	match_presences.clear()
+	matchmaker_ticket = null
+	match_ = await Online.socket.join_match_async(match_id)
+	if match_.is_exception():
+		print("[match_error]: >", OnlineMatch.match_.get_exception().message)
+		match_ = null
+		if emit_signals:
+			match_join_failed.emit()
+		return false
+	var label_fields: PackedStringArray = match_.label.split(" ")
+	for field in label_fields:
+		if field.begins_with("code="):
+			match_code = field.split("=")[1]
+			break
+	if emit_signals:
+		match_joined.emit()
+	return true
+
+
+func join_match(match_code: String) -> void:
+	var payload: Dictionary = {"match_code" : match_code}
+	var response = await Online.call_rpc_func("get_match_id_by_code", payload)
+	if response.is_exception():
+		print("[match_error]: >", response.get_exception().message)
+		match_ = null
+		match_join_failed.emit()
+		return
+	var json = JSON.parse_string(response.payload)
+	var match_id: String = json.get("match_id", "")
+	if match_id.is_empty():
+		print("[match_error]: Error retrieving match_id!")
+		match_ = null
+		match_join_failed.emit()
+		return
+	var joined: bool = await _join_match_by_id(match_id, true)
+	if not joined:
+		match_join_failed.emit()
+		match_ = null
+		return
+	match_joined.emit()
+	Online.socket.received_match_presence.connect(_on_received_match_presence)
 
 
 func _on_received_match_presence(match_presence: NakamaRTAPI.MatchPresenceEvent) -> void:
