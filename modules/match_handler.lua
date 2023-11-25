f93ec037930a55fa6ce757961bc84f14e4c7baf1
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-11-25 02:02:15 -0300

    feat(MatchHandler): implement 'ready' state receipt from client

--- modules/match_handler.lua
+++ modules/match_handler.lua
@@ -2,13 +2,16 @@ local nk = require("nakama")
 local utils = require("match_utils")
 local gen = require("generator")
 
+local READY_OP_CODE = 1
+local GAME_STARTING_OP_CODE = 2
+
 local M = {}
 
 function M.match_init(context, params)
 	local match_code = params.match_code
 	nk.logger_info(string.format("Match code: %s", tostring(match_code)))
 	local state = {
-		presences = {},
+		players = {},
 		match_code = match_code,
 		empty_ticks = 0
 	}
@@ -18,7 +21,6 @@ function M.match_init(context, params)
 		code = match_code
 	}
 	local label = nk.json_encode(label_data)
-	-- local label = "code=" .. match_code
 
 	return state, tick_rate, label
 end
@@ -35,7 +37,9 @@ function M.match_join(context, dispatcher, tick, state, presences)
 	--  ...
 	-- }
 	for _, presence in ipairs(presences) do
-		state.presences[presence.session_id] = presence
+		state.players[presence.session_id] = {}
+		state.players[presence.session_id]["presence"] = presence
+		state.players[presence.session_id]["is_ready"] = false
 	end
 	return state
   end
@@ -52,50 +56,82 @@ function M.match_join_attempt(context, dispatcher, tick, state, presence, metada
 	return state, acceptuser
 end
 
--- function M.match_join(context, dispatcher, tick, state, presences)
---   for _, presence in ipairs(presences) do
---     state.presences[presence.session_id] = presence
---   end
-
---   return state
--- end
-
 function M.match_leave(context, dispatcher, tick, state, presences)
 	for _, presence in ipairs(presences) do
-		state.presences[presence.session_id] = nil
+		state.players[presence.session_id] = nil
 	end
 
 	return state
 end
 
-function M.match_loop(context, dispatcher, tick, state, messages)
+local function _loop_check_empty_match(state, max_ticks)
 
-	local presences_count = table.getn(state.presences)
+	local presences_count = table.getn(state.players)
 	if presences_count == 0 then
 		state.empty_ticks = state.empty_ticks + 1
 	end
 
-	if state.empty_ticks > 100 then
+	if state.empty_ticks > max_ticks then
+		return true
+	else
+		return false
+	end
+end
+
+local function _loop_check_all_players_ready(state)
+
+	local all_ready = true
+	for _, player in pairs(state.players) do
+		if not player["is_ready"] then
+			all_ready = false
+			break
+		end
+	end
+
+	return all_ready
+end
+
+function M.match_loop(context, dispatcher, tick, state, messages)
+
+	if _loop_check_empty_match(state, 100) then
 		nk.logger_info(
 			string.format("Finishing match with code '%s' due to inactivity!", state.match_code))
 		utils.unregister_match_code(state.match_code)
 		return nil
 	end
 
+	for _, message in ipairs(messages) do
+		if message.op_code == READY_OP_CODE then
+			local session_id = message.sender.session_id
+			state.players[session_id]["is_ready"] = true
+			nk.logger_info(string.format("Player %s is ready", session_id))
+			dispatcher.broadcast_message(READY_OP_CODE, nk.json_encode(
+				{session_id = session_id, is_ready = true}
+			))
+
+			if _loop_check_all_players_ready(state) then
+				local message = "Game starting!"
+				dispatcher.broadcast_message(GAME_STARTING_OP_CODE, message)
+			end
+		end
+	end
+
 	return state
 end
 
 function M.match_terminate(context, dispatcher, tick, state, grace_seconds)
-  local message = "Server shutting down in " .. grace_seconds .. " seconds"
-  dispatcher.broadcast_message(2, message)
+	local message = "Server shutting down in " .. grace_seconds .. " seconds"
+	dispatcher.broadcast_message(2, message)
 
-  utils.unregister_match_code(state.match_code)
+	nk.logger_info(
+		string.format("Finishing match with code '%s' due to server shutdown!", state.match_code))
+	utils.unregister_match_code(state.match_code)
 
-  return nil
+	return nil
 end
 
 function M.match_signal(context, dispatcher, tick, state, data)
-  return state, "signal received: " .. data
+	return state, "signal received: " .. data
 end
 
 return M
